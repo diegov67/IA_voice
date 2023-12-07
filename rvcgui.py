566a38070739c8912ea876b011b35f2a39e1d4b4
diegov67 <vargas.diego@ficct.uagrm.edu.bo>
2023-12-07 10:58:18 -0400

    prueba servidor2

--- rvcgui.py
+++ rvcgui.py
@@ -20,20 +20,20 @@ os.environ["TEMP"] = tmp
 warnings.filterwarnings("ignore")
 torch.manual_seed(114514)
 
-from vc_infer_pipeline import VC
-from fairseq import checkpoint_utils
-from scipy.io import wavfile
-from my_utils import load_audio
-from infer_pack.models import SynthesizerTrnMs256NSFsid, SynthesizerTrnMs256NSFsid_nono
-from infer_pack.modelsv2 import SynthesizerTrnMs768NSFsid_nono, SynthesizerTrnMs768NSFsid
-from multiprocessing import cpu_count
-import threading
-from time import sleep
-from time import sleep
-import traceback
-import numpy as np
-import subprocess
-import zipfile
+# from vc_infer_pipeline import VC
+# from fairseq import checkpoint_utils
+# from scipy.io import wavfile
+# from my_utils import load_audio
+# from infer_pack.models import SynthesizerTrnMs256NSFsid, SynthesizerTrnMs256NSFsid_nono
+# from infer_pack.modelsv2 import SynthesizerTrnMs768NSFsid_nono, SynthesizerTrnMs768NSFsid
+# from multiprocessing import cpu_count
+# import threading
+# from time import sleep
+# from time import sleep
+# import traceback
+# import numpy as np
+# import subprocess
+# import zipfile
 from config import Config
 
 config = Config()
